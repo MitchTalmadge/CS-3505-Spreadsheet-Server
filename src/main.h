e48d3189d76fcf73ad3cda00a9a11371cacdfe6b
Mitch Talmadge <u1031378@utah.edu>
2018-04-05 23:32:39 -0600

    Attempted automatic download and compile of Boost libraries

--- src/main.h
+++ src/main.h
@@ -1,6 +1,8 @@
 #ifndef PIGRAMMERS_SPREADSHEET_SERVER_MAIN_H
 #define PIGRAMMERS_SPREADSHEET_SERVER_MAIN_H
 
+#include <boost/unordered_map.hpp>
+
 int main(int argc, char ** argv);
 
 #endif //PIGRAMMERS_SPREADSHEET_SERVER_MAIN_H

8c8ebbff788e4c3aa0b711a07c6c820ecac065ce
Mitch Talmadge <u1031378@utah.edu>
2018-04-06 10:48:56 -0600

    Clean up CMakeLists file

--- src/main.h
+++ src/main.h
@@ -1,10 +1,6 @@
 #ifndef PIGRAMMERS_SPREADSHEET_SERVER_MAIN_H
 #define PIGRAMMERS_SPREADSHEET_SERVER_MAIN_H
 
-#include <boost/date_time/gregorian/greg_date.hpp>
-
 int main(int argc, char **argv);
-
-boost::gregorian::date test();
 
 #endif //PIGRAMMERS_SPREADSHEET_SERVER_MAIN_H

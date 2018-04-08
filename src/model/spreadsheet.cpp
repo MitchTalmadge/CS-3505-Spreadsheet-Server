b8db56b4d5d2ecc71d6dabd0e64e9a30d9cebb63
Mitch Talmadge <u1031378@utah.edu>
2018-04-08 14:48:14 -0600

    Add cell normalization

--- src/model/spreadsheet.cpp
+++ src/model/spreadsheet.cpp
@@ -1,4 +1,5 @@
 #include "spreadsheet.h"
+#include <algorithm>
 
 spreadsheet::spreadsheet() = default;
 
@@ -10,10 +11,15 @@ void spreadsheet::save_to_file(const std::string &file_path) {
 
 }
 
-std::string spreadsheet::get_cell_contents( const std::string &  cell_name) {
-    return cell_contents_[cell_name];
+std::string spreadsheet::get_cell_contents(const std::string &cell_name) {
+    return cell_contents_[normalize_cell_name(cell_name)];
 }
 
-void spreadsheet::set_cell_contents( const std::string &  cell_name, const std::string & contents) {
-    cell_contents_[cell_name] = contents;
+void spreadsheet::set_cell_contents(const std::string &cell_name, const std::string &contents) {
+    cell_contents_[normalize_cell_name(cell_name)] = contents;
+}
+
+std::string spreadsheet::normalize_cell_name(std::string cellName) {
+    std::transform(cellName.begin(), cellName.end(), cellName.begin(), ::toupper);
+    return cellName;
 }

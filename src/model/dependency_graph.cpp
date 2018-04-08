290332d0898cf094a43b36f7eeea52dd191b3341
Mitch Talmadge <u1031378@utah.edu>
2018-04-08 04:31:06 -0600

    Fix dependency graph using non-pointer variables

--- src/model/dependency_graph.cpp
+++ src/model/dependency_graph.cpp
@@ -4,49 +4,49 @@ int dependency_graph::size() const {
     return size_;
 }
 
-std::set <std::string> dependency_graph::get_dependees(const std::string& node) {
+std::set<std::string> dependency_graph::get_dependees(const std::string &node) {
     return dependees_[node];
 }
 
-std::set <std::string> dependency_graph::get_dependents(const std::string& node) {
+std::set<std::string> dependency_graph::get_dependents(const std::string &node) {
     return dependents_[node];
 }
 
-void dependency_graph::add_dependency(const std::string& node, const std::string& dependent_node) {
-    std::set <std::string> dependent_node_dependees = dependees_[dependent_node];
-    std::set <std::string> node_dependents = dependents_[node];
+void dependency_graph::add_dependency(const std::string &node, const std::string &dependent_node) {
+    auto *dependent_node_dependees = &dependees_[dependent_node];
+    auto *node_dependents = &dependents_[node];
 
     // Attempt insertion of the node and its dependent.
-    bool inserted = dependent_node_dependees.insert(node).second;
-    inserted &= node_dependents.insert(dependent_node).second;
+    bool inserted = dependent_node_dependees->insert(node).second;
+    inserted &= node_dependents->insert(dependent_node).second;
 
     // Increase number of pairs if insertion was successful.
     if (inserted)
         size_++;
 }
 
-void dependency_graph::remove_dependency(const std::string& node, const std::string& dependent_node) {
-    std::set <std::string> node_dependees = dependees_[dependent_node];
-    std::set <std::string> node_dependents = dependents_[node];
+void dependency_graph::remove_dependency(const std::string &node, const std::string &dependent_node) {
+    auto *dependent_node_dependees = &dependees_[dependent_node];
+    auto *node_dependents = &dependents_[node];
 
-	auto removed = false;
+    auto removed = false;
 
     // Remove node
-    auto nodeToRemove = node_dependees.find(node);
-    if (nodeToRemove != node_dependees.end()) {
-        node_dependees.erase(nodeToRemove);
+    auto nodeToRemove = dependent_node_dependees->find(node);
+    if (nodeToRemove != dependent_node_dependees->end()) {
+        dependent_node_dependees->erase(nodeToRemove);
         removed = true;
     }
 
     // Remove node's dependent
-    nodeToRemove = node_dependents.find(dependent_node);
-    if (nodeToRemove != node_dependents.end()) {
-        node_dependees.erase(nodeToRemove);
+    nodeToRemove = node_dependents->find(dependent_node);
+    if (nodeToRemove != node_dependents->end()) {
+        dependent_node_dependees->erase(nodeToRemove);
         removed = true;
     }
 
     // Decrease number of pairs if removal was successful.
-    if(removed) {
+    if (removed) {
         size_--;
     }
 }

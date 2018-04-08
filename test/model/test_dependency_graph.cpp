e50e76ff49fd55984724531706a0617b898ebdcc
Mitch Talmadge <u1031378@utah.edu>
2018-04-08 03:27:14 -0600

    Add missing semicolons :')

--- test/model/test_dependency_graph.cpp
+++ test/model/test_dependency_graph.cpp
@@ -8,4 +8,21 @@ TEST(DependencyGraphTest, GraphStartsEmpty) {
     dependency_graph graph;
 
     EXPECT_EQ(0, graph.size());
+    EXPECT_EQ(0, graph.get_dependees("A1").size());
+    EXPECT_EQ(0, graph.get_dependents("A1").size());
+}
+
+/**
+ * Tests that a single dependency can be added and retrieved from the graph.
+ */
+TEST(DependencyGraphTest, AddOneDependency) {
+    dependency_graph graph;
+
+    graph.add_dependency("A1", "A2");
+
+    EXPECT_EQ(1, graph.get_dependees("A2").size());
+    EXPECT_EQ("A1", graph.get_dependees("A2")[0]);
+
+    EXPECT_EQ(1, graph.get_dependents("A1").size());
+    EXPECT_EQ("A2", graph.get_dependents("A1")[0]);
 }

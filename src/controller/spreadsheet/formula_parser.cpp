5a7246301b360d4bf8b152df02623794dc217548
Mitch Talmadge <u1031378@utah.edu>
2018-04-08 16:22:14 -0600

    PIG-10: Add formula parser structure

---
+++ src/controller/spreadsheet/formula_parser.cpp
@@ -0,0 +1,9 @@
+#include "formula_parser.h"
+
+bool formula_parser::is_valid(const std::string &formula) {
+    return false;
+}
+
+std::set<std::string> formula_parser::find_dependents(const std::string &formula) {
+    return std::set<std::string>();
+}
---
+++ src/controller/spreadsheet/formula_parser.h
@@ -0,0 +1,28 @@
+#ifndef PIGRAMMERS_SPREADSHEET_SERVER_FORMULA_PARSER_H
+#define PIGRAMMERS_SPREADSHEET_SERVER_FORMULA_PARSER_H
+
+#include <string>
+#include <set>
+
+class formula_parser {
+
+public:
+
+    /**
+     * Determines if the given formula is formatted correctly.
+     * @param formula The formula to check, such as "=A1 + 10"
+     * @return True if the formula is formatted correctly, false otherwise.
+     */
+    static bool is_valid(const std::string &formula);
+
+    /**
+     * Finds the names of all cells that depend on this formula.
+     * @param formula The formula to search.
+     * @return A set containing the names of the cells which depend on the formula.
+     */
+    static std::set<std::string> find_dependents(const std::string &formula);
+
+};
+
+
+#endif //PIGRAMMERS_SPREADSHEET_SERVER_FORMULA_PARSER_H

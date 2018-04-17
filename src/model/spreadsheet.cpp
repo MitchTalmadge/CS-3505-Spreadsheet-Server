f81db4b9f8a06f8b0c08d50a256baed2ba8c516b
Mark Van der Merwe <vandermerwem2@gmail.com>
2018-04-17 16:58:21 -0600

    Add undo functionality

--- src/model/spreadsheet.cpp
+++ src/model/spreadsheet.cpp
@@ -17,5 +17,31 @@ std::string spreadsheet::get_cell_contents(const std::string &cell_name) {
 }
 
 void spreadsheet::set_cell_contents(const std::string &cell_name, const std::string &contents) {
+  // Before we write the new value in, save the old value to the undo stack for this spreadsheet.
+  std::old_value = cell_contents_[spreadsheet_controller::normalize_cell_name(cell_name)];
+  
+  cell_value old_cell;
+  old_cell.cell_name = cell_name;
+  old_cell.value = old_value;
+
+  // Push value onto the undo history.
+  undo_history_.push(old_cell);
+
     cell_contents_[spreadsheet_controller::normalize_cell_name(cell_name)] = contents;
+}
+
+void spreadsheet::undo() {
+  // If history exists - history is maintained for current server session of this spreadsheet.
+  if (!undo_history_.empty()) {
+    cell_value undo = undo_history_.top();
+
+    std::string cell_name = undo.cell_name;
+    std::string value = undo.value;
+
+    // Set value ourselves and DON'T place current value on undo stack - undos are destructive.
+    cell_contents_[spreadsheet_controller::normalize_cell_name(cell_name)] = value;
+
+    // Remove element from undo stack.
+    undo_history_.pop();
+  }
 }

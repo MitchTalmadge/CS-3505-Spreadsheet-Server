b28d8c42301eb1a0077a706d5a94c5e3954dd3d8
Mitch Talmadge <u1031378@utah.edu>
2018-04-18 23:37:43 -0600

    PIG-16: Fix creation of new spreadsheets.

--- src/controller/spreadsheet/spreadsheet_controller.cpp
+++ src/controller/spreadsheet/spreadsheet_controller.cpp
@@ -90,9 +90,19 @@ void spreadsheet_controller::parse_inbound_packet(inbound_packet &packet, const 
     case inbound_packet::LOAD: {
       auto load_packet = dynamic_cast<inbound_load_packet &>(packet);
 
-      // If a spreadsheet is not loaded, it will be created automatically via the operator[] accessor of the map.
-      data_container_.new_outbound_packet(packet.get_socket_id(),
-                                          *new outbound_full_state_packet(active_spreadsheets_[load_packet.get_spreadsheet_name()]->get_non_empty_cells()));
+      // Try to find an existing spreadsheet.
+      auto item = active_spreadsheets_.find(load_packet.get_spreadsheet_name());
+      if (item != active_spreadsheets_.end()) {
+        // Spreadsheet found.
+        data_container_.new_outbound_packet(packet.get_socket_id(),
+                                            *new outbound_full_state_packet((*item).second->get_non_empty_cells()));
+      } else {
+        // Create new spreadsheet.
+        active_spreadsheets_[load_packet.get_spreadsheet_name()] = new spreadsheet;
+        data_container_.new_outbound_packet(packet.get_socket_id(),
+                                            *new outbound_full_state_packet(active_spreadsheets_[load_packet.get_spreadsheet_name()]->get_non_empty_cells()));
+      }
+
       break;
     }
     case inbound_packet::FOCUS: {
